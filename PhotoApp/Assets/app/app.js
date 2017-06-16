e6714b14a6f5b737d6f5b266d80959b33d5093ba
Mattiu86 <mattiu@o2.pl>
2017-06-16 22:46:33 +0200

    Your Message

---
+++ PhotoApp/Assets/app/app.js
@@ -0,0 +1,36 @@
+﻿var app = angular.module('app', [
+    'ngRoute',
+    'ngCookies',
+    'home',
+    'ui.bootstrap'
+]);
+
+
+
+
+app.config(['$provide', '$routeProvider', '$httpProvider', function ($provide, $routeProvider, $httpProvider) {
+
+    //================================================
+    // Routes
+    //================================================
+    $routeProvider.when('/home', {
+        templateUrl: 'App/Home',
+        controller: 'homeCtrl'
+    }).otherwise({
+        redirectTo: '/home'
+    });
+}]);
+
+app.run(['$http', '$cookies', '$cookieStore', function ($http, $cookies, $cookieStore) {
+    //If a token exists in the cookie, load it after the app is loaded, so that the application can maintain the authenticated state.
+    $http.defaults.headers.common.Authorization = 'Bearer ' + $cookieStore.get('_Token');
+    $http.defaults.headers.common.RefreshToken = $cookieStore.get('_RefreshToken');
+}]);
+
+
+//GLOBAL FUNCTIONS - pretty much a root/global controller.
+app.run(['$rootScope', '$http', '$cookies', '$cookieStore', function ($rootScope, $http, $cookies, $cookieStore) {
+         
+  
+}]);
+
---
+++ PhotoApp/Assets/app/controllers/homeCtrl.js
@@ -0,0 +1,115 @@
+﻿var appHome = angular.module('home', ['ui.bootstrap']);
+
+appHome.controller('homeCtrl', ['$scope', '$http', 'PhotoService', function ($scope, $http, PhotoService) {
+
+    PhotoService.loadData();
+    $scope.PhotoService = PhotoService;
+    $scope.currentPage = 0;
+    $scope.pageSize = 25;
+    $scope.numbers= [4,3.5,4.1324,2.1]
+
+    $scope.pages = function () {
+        return Math.ceil($scope.PhotoService.allItems.length / $scope.pageSize);
+    }
+}])
+
+appHome.factory('PhotoService', ['$http','$filter', function ($http, $filter) {
+
+    var Source = {};
+    //Source.items = [];
+    Source.allItems = [];
+    Source.loadData = function () {
+
+        return $http.get('https://jsonplaceholder.typicode.com/photos')
+            .then(function (response) {
+                Source.allItems = response.data;
+                //Source.items = $filter('limitTo')(Source.allItems, 25, 0);
+            });
+    };
+
+    return Source;
+}]);
+
+
+appHome.filter('startFrom', function () {
+    return function (input, startPoint) {
+        startPoint = +startPoint;
+        return input.slice(startPoint);
+    }
+});
+
+appHome.filter('mathPower', function () {
+    return function (number) {
+
+        var n = parseFloat(number.toFixed(2))
+        var base = Math.floor(n)
+        var exponent = 0;
+        var splitNumber = n.toString().split(".");
+        if (splitNumber.length > 1)
+        {
+            exponent = n.toString().split(".")[1];
+        }
+        
+        return Math.pow(base, exponent);
+    }
+})
+
+appHome.filter('mathPowerBase', function () {
+    return function (number) {
+               
+        return Math.floor(number);       
+    }
+})
+
+appHome.filter('mathPowerExponent', function () {
+    return function (number) {
+
+        var n = parseFloat(number.toFixed(2));
+        var base = Math.floor(n);
+        var exponent = 0;
+        var splitNumber = n.toString().split(".");
+        if (splitNumber.length > 1) {
+            exponent = n.toString().split(".")[1];
+        }
+        return exponent;
+    }
+})
+
+
+appHome.directive('photoAppImage', ['$uibModal', function ($uibModal) {
+
+    var controller = ['$scope', function ($scope) {
+
+        $scope.modal = null;
+
+        $scope.openModal = function () {
+            $scope.modal = $uibModal.open({
+                template: '<div class="modal-header"><h3 class="modal-title">{{item.title}}</h3></div>' +
+                '<div class="modal-body">' +
+                '<div>' +
+                '<ul class="list-group"><li class="list-group-item">ID {{item.id}}</li>' +
+                '<li class="list-group-item">Album ID {{item.albumId}}</li>' +
+                '</ul>' +
+                '</div>' +
+                '<img class="img-responsive center-block" ng-src="{{item.url}}" alt=""/></div>' +
+                '<div class="modal-footer"><button class="btn btn-warning" type="button" ng-click="cancel()">Cancel</button></div>',
+                size: 'lg',
+                windowClass: 'confirm-window',
+                scope: $scope
+            });
+        };
+
+        $scope.cancel = function () {
+            $scope.modal.close('cancel');
+        }
+    }]
+
+    return {
+        restrict: 'E',
+        scope: {
+            item: '=item'
+        },
+        template: '<img ng-click="openModal()" class="img-responsive img-thumbnail" ng-src="{{item.thumbnailUrl}}" alt=""/><div class="text-info"> {{item.title }}</div >',
+        controller: controller
+    };
+}]);
